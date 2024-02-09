bf6303b5bfa33e516498961ebaba206147f50e43
Anzhelika <angelikachashchina@gmail.com>
2024-02-09 14:41:37 +0300

    добавлены исключения для cors

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ mongoose.connect(`${MONGO_URL}`)
   .then(() => console.log('база данных подключена'))
   .catch((err) => console.error(err));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use(express.json());
 app.use(cors());
 app.use(requestLogger); // подключаем логгер запросов
