1090897fe1d3c9aade3ea65043346fb3832a937f
Anzhelika <angelikachashchina@gmail.com>
2024-02-09 15:27:00 +0300

    добавлены исключения для cors

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
-const cors = require('./middlewares/cors');
 const errorHandler = require('./middlewares/errorHandler');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -21,8 +21,20 @@ mongoose.connect(`${MONGO_URL}`)
   .then(() => console.log('база данных подключена'))
   .catch((err) => console.error(err));
 
+const options = {
+  origin: [
+    'http://localhost:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(options)); // ПЕРВЫМ!
+
 app.use(express.json());
-app.use(cors);
 app.use(requestLogger); // подключаем логгер запросов
 
 app.use(helmet());
